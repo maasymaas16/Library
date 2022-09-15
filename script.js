a0ea9dc7f3774c0525b7def19ff344a2e0dccb26
maasymaas16 <maasymaas16@gmail.com>
2022-09-14 22:27:05 -0600

    Added javascript for creating and displaying 'cards' for each book in the library

--- script.js
+++ script.js
@@ -32,8 +32,26 @@ myLibrary.push(Eragon);
 
 function openForm() {
     document.getElementById("myForm").style.display = "block";
+
   }
   
   function closeForm() {
     document.getElementById("myForm").style.display = "none";
   }
+
+myLibrary.forEach(book => {
+        const div = document.createElement('div');
+        div.classList.add('bookcard');
+        const title = document.createElement('div');
+        const author = document.createElement('div');
+        const pages = document.createElement('div');
+        title.innerHTML = book.title;
+        author.innerHTML = book.author;
+        pages.innerHTML = book.pages;
+        div.appendChild(title);
+        div.appendChild(author);
+        div.appendChild(pages);
+        const bookcards = document.querySelector('.bookcards-cont');
+        bookcards.appendChild(div);
+
+})
