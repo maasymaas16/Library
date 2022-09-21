822fed262ac9683daed40b22e48451c1c1735fa0
maasymaas16 <maasymaas16@gmail.com>
2022-09-20 21:18:09 -0600

    Fixed code to have page automatically add new card when a new book is added

--- script.js
+++ script.js
@@ -7,51 +7,54 @@ function Book(title, author, pages, read){
     this.author = author;
     this.read = read;
 
-    this.info = function() {
-        if (read === true) {
-            return title + ' by ' + author + ', ' + pages + ' pages, already read';
-        } else {
-            return title + ' by ' + author + ', ' + pages + ' pages, not yet read';
-        }
-    }
 }
 
 function addBookToLibrary() {
-
+  myLibrary.push(getBookFromInput());
 }
-
-function displayBooks() {
-
-}
-
-const Hobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 255, false);
-const Eragon = new Book('Eragon', 'Christopher Paolini', 488, true);
-
-myLibrary.push(Hobbit);
-myLibrary.push(Eragon);
 
 function openForm() {
     document.getElementById("myForm").style.display = "block";
 
   }
-  
-  function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-  }
 
-myLibrary.forEach(book => {
-        const div = document.createElement('div');
-        div.classList.add('bookcard');
-        const title = document.createElement('div');
-        const author = document.createElement('div');
-        const pages = document.createElement('div');
-        title.innerHTML = book.title;
-        author.innerHTML = book.author;
-        pages.innerHTML = book.pages;
-        div.appendChild(title);
-        div.appendChild(author);
-        div.appendChild(pages);
-        const bookcards = document.querySelector('.bookcards-cont');
-        bookcards.appendChild(div);
+const getBookFromInput = () =>  {
+    const title = document.getElementById('title').value;
+    console.log(title);
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    return new Book(title, author, pages, true);
+} 
 
-})
+function updateLibrary () {
+  myLibrary.forEach(book => {
+    const div = document.createElement('div');
+    div.classList.add('bookcard');
+    const btitle = document.createElement('div');
+    const bauthor = document.createElement('div');
+    const bpages = document.createElement('div');
+    btitle.innerHTML = book.title;
+    bauthor.innerHTML = book.author;
+    bpages.innerHTML = book.pages;
+    div.appendChild(btitle);
+    div.appendChild(bauthor);
+    div.appendChild(bpages);
+    const bookcards = document.querySelector('.bookcards-cont');
+    bookcards.appendChild(div);
+  })
+};
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+  clearCards();
+  addBookToLibrary();
+  updateLibrary();
+}
+
+function clearCards () {
+  document.querySelector(".bookcards-cont").innerHTML = "";
+}
+
+
+
+
