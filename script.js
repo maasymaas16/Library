524392a8b584b2d771ca54b8b953386dddcc0f98
maasymaas16 <maasymaas16@gmail.com>
2022-09-22 21:42:01 -0600

    Added close button to form popup

--- script.js
+++ script.js
@@ -20,9 +20,10 @@ function openForm() {
 
 const getBookFromInput = () =>  {
     const title = document.getElementById('title').value;
-    console.log(title);
     const author = document.getElementById('author').value;
     const pages = document.getElementById('pages').value;
+    const read = document.getElementById('read-check').checked;
+    console.log(read);
     return new Book(title, author, pages, true);
 } 
 
@@ -30,6 +31,9 @@ function updateLibrary () {
   myLibrary.forEach(book => {
     const div = document.createElement('div');
     div.classList.add('bookcard');
+
+    const buttonrow = document.createElement('div');
+    buttonrow.classList.add('buttonrow');
 
     const btitle = document.createElement('div');
     const bauthor = document.createElement('div');
@@ -43,10 +47,24 @@ function updateLibrary () {
     div.appendChild(bauthor);
     div.appendChild(bpages);
 
+    const read = document.getElementById('read-check').checked;
+
     const remove = document.createElement('button');
     remove.classList.add("remove-btn");
+
+    const readbtn = document.createElement('button');
+    if (read){
+      readbtn.classList.add('read-btn');
+      readbtn.innerHTML = 'Read';
+    } else {
+      readbtn.classList.add('notread-btn');
+      readbtn.innerHTML = 'Not Read';
+    }
+
     remove.innerHTML = "Remove";
-    div.appendChild(remove);
+    buttonrow.appendChild(remove);
+    buttonrow.appendChild(readbtn);
+    div.appendChild(buttonrow);
 
     const bookcards = document.querySelector('.bookcards-cont');
     bookcards.appendChild(div);
@@ -58,10 +76,19 @@ function closeForm() {
   clearCards();
   addBookToLibrary();
   updateLibrary();
+
+}
+
+function exitForm () {
+  document.getElementById("myForm").style.display = "none";
 }
 
 function clearCards () {
   document.querySelector(".bookcards-cont").innerHTML = "";
+}
+
+function removeBook (book) {
+
 }
 
 
